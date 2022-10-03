d1c1c7c95313e5cc8e0a475a677c615f74e17c16
Kevin Branch <kevin@branchnetconsulting.com>
2022-10-03 11:00:32 -0400

    Update check-osquery.sh

--- check-osquery.sh
+++ check-osquery.sh
@@ -33,7 +33,7 @@ TargetOsqueryVersion=`cat /var/ossec/etc/shared/osquery-target-version`
 TargetOsqueryVersion=`echo $TargetOsqueryVersion | sed 's/\s*\([^\s]\+\)\s*/\1/'`
 
 sleep 5
-if [[ ! `pgrep osqueryd` ]]; then
+if [[ ! `ps auxw | grep -v grep | egrep "osqueryd.*osquery-linux.conf"` ]]; then
     echo "$TargetOsqueryVersion"
     exit
 fi
